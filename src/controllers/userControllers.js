daef3d2835485d7897612c2601304f1756272286
Roberto Ochoa <oc_005@hotmail.com>
2023-04-26 11:00:48 -0300

    llamo a la función de envío de mail cuando se crea el user

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,84 +1,93 @@
-const {User} = require('../DB_connection')
-const {Role} = require('../DB_connection')
+const { User } = require("../DB_connection");
+const { Role } = require("../DB_connection");
+const { notificationNewUser } = require("./notificationNewUser");
 
-const getAllUsers = async () =>{
-    const users = await User.findAll({
-        where:{
-            active: true
-        },
-        include:{
-            model:Role,
-            attributes: ["name"],
-                through: {
-                    attributes: [] 
-                }
-        }
-    })
+const getAllUsers = async () => {
+  const users = await User.findAll({
+    where: {
+      active: true,
+    },
+    include: {
+      model: Role,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
 
-    return users
-}
+  return users;
+};
 
-const getDetailUser = async (name) =>{
-    const userDetail = await User.findOne({
-        where:{
-            name: name,
-            active: true
-        },
-        include:{
-            model:Role,
-            attributes: ["name"],
-                through: {
-                    attributes: [] 
-            }
-        }
-    })
-    return userDetail
-}
+const getDetailUser = async (name) => {
+  const userDetail = await User.findOne({
+    where: {
+      name: name,
+      active: true,
+    },
+    include: {
+      model: Role,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+  return userDetail;
+};
 
-const createUser = async (nickname, picture, email) =>{
+const createUser = async (nickname, picture, email) => {
+  if (!email) email = "not specified";
 
-    if(!email) email = 'not specified'
+  const [user, created] = await User.findOrCreate({
+    where: { email: email },
+    defaults: { name: nickname, picture: picture, email: email },
+  });
 
-    const [user, created ] = await User.findOrCreate({
-        where: { email: email },
-        defaults: { name: nickname, picture: picture, email: email }
-      });
+  const role = await Role.findOne({
+    where: { name: "user" },
+  });
 
-    const role = await Role.findOne({
-        where:{name: 'user'}
-    })
-
-    await user.addRole(role)
-
-    return user;
-}
+  await user.addRole(role);
+  notificationNewUser(email, user);
+  return user;
+};
 
 const addAdminRole = async (name) => {
-    const user = await User.findOne({
-        where: { name: name }        
-    })
-    const role = await Role.findOne({
-        where:{ name: 'admin' }
-    })
+  const user = await User.findOne({
+    where: { name: name },
+  });
+  const role = await Role.findOne({
+    where: { name: "admin" },
+  });
 
-    if (!user) {
-        throw new Error('There is no user does not exists')
-    }
+  if (!user) {
+    throw new Error("There is no user does not exists");
+  }
 
-    await user.addRole(role)
+  await user.addRole(role);
 
-    return user;
-}
+  return user;
+};
 
 const updateProfilePic = async (name, newPic) => {
-    const user = await User.findOne({
-        where: {name : name}
-    })
-     user.picture=newPic;
+  const user = await User.findOne({
+    where: { name: name },
+  });
+  user.picture = newPic;
 
-     user.save();
-     
-     return {newPicture :user.picture, message:'Profile pic successfully updated'};
-  }
-  
-module.exports = {getAllUsers, getDetailUser, createUser, updateProfilePic, addAdminRole}
+  user.save();
+
+  return {
+    newPicture: user.picture,
+    message: "Profile pic successfully updated",
+  };
+};
+
+module.exports = {
+  getAllUsers,
+  getDetailUser,
+  createUser,
+  updateProfilePic,
+  addAdminRole,
+};
