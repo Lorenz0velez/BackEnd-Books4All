1938f7cfd6cd27cdd7901e18e79980d36f739aa6
Roberto Ochoa <oc_005@hotmail.com>
2023-04-26 10:59:17 -0300

    template new user + env USER

--- src/controllers/notificationNewUser.js
+++ src/controllers/notificationNewUser.js
@@ -1,9 +1,9 @@
 const { transporter } = require("../mailer");
-const { MAIL } = process.env;
+const { USER } = process.env;
 
 const notificationNewUser = async (email, user) => {
   await transporter.sendMail({
-    from: `"Books4All"<${MAIL}>`, //.env
+    from: `"Books4All"<${USER}>`, //.env
     to: `${email}`,
     subject: "Books4All: New User",
     html: `<p> The user ${user.dataValues.name} has been successfully created.<p/>`,
