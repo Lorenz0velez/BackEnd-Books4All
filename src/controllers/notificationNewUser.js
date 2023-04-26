c811b7ca1d70e37bcf934cccf6e29e318d386678
Roberto Ochoa <oc_005@hotmail.com>
2023-04-26 13:05:18 -0300

    condicional en userController para que solo mande la notificacion si el usuario es nuevo

--- src/controllers/notificationNewUser.js
+++ src/controllers/notificationNewUser.js
@@ -3,10 +3,10 @@ const { USER } = process.env;
 
 const notificationNewUser = async (email, user) => {
   await transporter.sendMail({
-    from: `"Books4All"<${USER}>`, //.env
-    to: `${email}`,
+    from: `"Books4All prueba desconocido"<${USER}>`, //.env
+    to: /* `${email}` */ "robertoochoa.dev@gmail.com",
     subject: "Books4All: New User",
-    html: `<p> The user ${user.dataValues.name} has been successfully created.<p/>`,
+    html: `<p> The user ${user} has been successfully created.<p/>`,
   });
 };
 
