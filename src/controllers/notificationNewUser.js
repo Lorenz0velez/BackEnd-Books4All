36fc598db68b700423bb189c0fc83feb110d7a8c
Roberto Ochoa <oc_005@hotmail.com>
2023-04-28 14:46:01 -0300

    styles new user

--- src/controllers/notificationNewUser.js
+++ src/controllers/notificationNewUser.js
@@ -21,41 +21,41 @@ const notificationNewUser = async (email, user) => {
     to: `${email}`,
     subject: "Books4All: New User",
     html: `
-      <html>
-        <head>
-          <style>
-            body {
-              background-color: rgb(33, 37, 41);
-              color: white;
-              font-family: Arial, sans-serif;
-            }
-            
-           header {
-              background-color: black;
-              padding: 20px;
-              display: flex;
-              align-items: center;
-              justify-content: space-between;
-            }
-            
-            img {
-              height: 50px;
-            }
-            
-            h1 {
-              font-size: 24px;
-              margin: 0;
-            }
-          </style>
-        </head>
-        <body>
-          <header>
-            <img src="https://cdn.discordapp.com/attachments/1091730813529374777/1098218919065559060/books4all.png" alt="Logo de Books4All">
-            <h1>Bienvenido a Books4All</h1>
-          </header>
-          <p>El usuario ${user} ha sido creado exitosamente.</p>
-        </body>
-      </html>
+    <html>
+    <head>
+      <style>
+        body {
+          background-color: rgb(33, 37, 41);
+          color: white;
+          font-family: Arial, sans-serif;
+        }
+        
+       header {
+          background-color: black;
+          padding: 20px;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+        }
+        
+        img {
+          height: 50px;
+        }
+        
+        h1 {
+          font-size: 24px;
+          margin: 0;
+        }
+      </style>
+    </head>
+    <body>
+      <header>
+        <img src="https://cdn.discordapp.com/attachments/1091730813529374777/1098218919065559060/books4all.png" alt="Logo de Books4All">
+        <h1>Bienvenido a Books4All!</h1>
+      </header>
+      <h3>El usuario ${user} ha sido creado exitosamente.</h3>
+    </body>
+  </html>
     `,
   });
 };
