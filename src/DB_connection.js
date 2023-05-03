f914a7c01d68655668630b44d42a0254fbad2d59
Débora Navarro <navarrodeboracecilia@gmail.com>
2023-05-03 05:03:49 -0300

    DB to force true and alter true, to load Latest News

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY, DB_PORT } = process.env;
-const BookModel = require('./Models/Book');
-const ReviewsModel = require('./Models/Reviews');
-const UserModel = require('./Models/User');
-const RoleModel = require('./Models/Role');
-const Boughtmodel = require('./Models/Bought');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY, DB_PORT } =
+  process.env;
+const BookModel = require("./Models/Book");
+const ReviewsModel = require("./Models/Reviews");
+const UserModel = require("./Models/User");
+const RoleModel = require("./Models/Role");
+const Boughtmodel = require("./Models/Bought");
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-    {
-        logging: false,
-        alter: true,
-        native: false
-    }
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false,
+    alter: true,
+    native: false,
+  }
 );
 // const sequelize = new Sequelize(DB_DEPLOY, {
 //     logging: false, // set to console.log to see the raw SQL queries
@@ -27,23 +29,23 @@ Boughtmodel(sequelize);
 
 //Relaciones entidad
 
-const { Book, Reviews, User, Role, Bought } = sequelize.models
+const { Book, Reviews, User, Role, Bought } = sequelize.models;
 
 Book.hasMany(Reviews);
 Reviews.belongsTo(Book);
 
 User.hasMany(Reviews);
 Reviews.belongsTo(User);
-User.belongsToMany(Role, { through: 'users_roles' });
-Role.belongsToMany(User, { through: 'users_roles' });
+User.belongsToMany(Role, { through: "users_roles" });
+Role.belongsToMany(User, { through: "users_roles" });
 
-User.belongsToMany(Book, { through: 'books_users' });
-Book.belongsToMany(User, { through: 'books_users' });
+User.belongsToMany(Book, { through: "books_users" });
+Book.belongsToMany(User, { through: "books_users" });
 
 User.hasMany(Bought);
 Bought.belongsTo(User);
 
 module.exports = {
-    ...sequelize.models,
-    sequelize
-}
+  ...sequelize.models,
+  sequelize,
+};
